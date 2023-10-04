ae8a6e323b5a929a966680a299849f917cf4e40c
Dmitrii <navssane@gmail.com>
2023-10-05 04:51:32 +0700

    Added navigation buttons

--- AnimeScheduler/Views/Full Schedule Table View/View/FullScheduleTableViewController.swift
+++ AnimeScheduler/Views/Full Schedule Table View/View/FullScheduleTableViewController.swift
@@ -27,19 +27,34 @@ class FullScheduleTableViewController: UITableViewController{
         //View to show on screen when no tasks are present
         let view = UIView()
         view.backgroundColor = #colorLiteral(red: 0.6549019608, green: 0.5098039216, blue: 0.9254901961, alpha: 1)
-        //let image = UIImage(named: "ai_dance_character")
-        //view.image = image
-        /*let label = UILabel(frame: CGRect(x: 0, y: 0, width: 350, height: 21))
-        label.center = CGPoint(x: 120, y: -40)
-        label.numberOfLines = 2
-        label.textAlignment = .center
-        label.lineBreakMode = .byWordWrapping
-        label.text = "No tasks! Let's not spoil\n the mood and keep the app clean"
-        label.sizeToFit()
-    
-        view.addSubview(label)*/
-        view.translatesAutoresizingMaskIntoConstraints = false
         return view
+    }()
+    private let buttonFS: UIButton = {
+        //Creating a floating button
+        let buttonFS = UIButton(frame: CGRect(x: 40, y: 5, width: 60, height: 60))
+        buttonFS.layer.masksToBounds = true
+        let image = UIImage(systemName: "list.bullet.rectangle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium))
+        buttonFS.setImage(image, for: .normal)
+        buttonFS.tintColor = .white
+        return buttonFS
+    }()
+    private let buttonMS: UIButton = {
+        //Creating a floating button
+        let buttonFS = UIButton(frame: CGRect(x: 40, y: 5, width: 60, height: 60))
+        buttonFS.layer.masksToBounds = true
+        let image = UIImage(systemName: "heart", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium))
+        buttonFS.setImage(image, for: .normal)
+        buttonFS.tintColor = .white
+        return buttonFS
+    }()
+    private let buttonSettings: UIButton = {
+        //Creating a floating button
+        let buttonFS = UIButton(frame: CGRect(x: 40, y: 5, width: 60, height: 60))
+        buttonFS.layer.masksToBounds = true
+        let image = UIImage(systemName: "gear", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium))
+        buttonFS.setImage(image, for: .normal)
+        buttonFS.tintColor = .white
+        return buttonFS
     }()
     
     override func viewDidLoad() {
@@ -53,8 +68,14 @@ class FullScheduleTableViewController: UITableViewController{
         //Configurator.configure(viewController: self)
         interactor?.getScheduleData()
         bottomNavBar.frame = CGRect(x: 0, y: view.frame.size.height - 120, width: view.frame.size.width, height: 80)
+        
         bottomNavBar.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(bottomNavBar)
+        bottomNavBar.addSubview(buttonFS)
+        bottomNavBar.addSubview(buttonMS)
+        bottomNavBar.addSubview(buttonSettings)
+        buttonMS.frame =  CGRect(x: view.frame.size.width/2 - 30, y: 5, width: 60, height: 60)
+        buttonSettings.frame = CGRect(x: view.frame.size.width - 100, y: 5, width: 60, height: 60)
         //removing nav bar
         self.navigationController?.isNavigationBarHidden = true
     }
